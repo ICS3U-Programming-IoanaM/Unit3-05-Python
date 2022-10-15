03123e8e4fb9c9ec59b30dce02a7dba63e9b8d20
Ioana Marinescu <sh ./setup.shioana.marinescu@stu.ocsb.ca>
2022-10-15 01:50:40 +0000

    :(

---
+++ month.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# Copyright (c) 2022 Ioana Marinescu All rights reserved.
+# Created By: Ioana Marinescu
+#
+# Date: Oct. 14, 2022
+# tells user what month is associated with a their chosen number from 1 - 12
+
+
+def month_association(month):
+    # checks what number is associated with which month and displays the month
+    months = {
+        1: print("1 is January."),
+        2: print("2 is February."),
+        3: print("3 is March."),
+        4: print("4 is April."),
+        5: print("5 is May."),
+        6: print("6 is June."),
+        7: print("7 is July."),
+        8: print("8 is August."),
+        9: print("9 is September."),
+        10: print("10 is October."),
+        11: print("11 is November."),
+        12: print("12 is December.")
+    }
+    return months.get(month, "Error. {} does not represent a month.".format(month))
+
+def main():
+    # variables
+    month_num = int(input("Enter in a number from 1 - 12: "))
+
+    # prints out month association
+    print(month_association(month_num))
+
+if __name__ == "__main__":
+    main()
